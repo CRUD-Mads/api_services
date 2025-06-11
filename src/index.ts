bea9d16f7a197b49b1f1679f10bee987f254d238
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-06-11 08:45:16 -0300

    feat: :sparkles: Criando a rota de checagem de saúde

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
-import express from 'express';
-import router from './routes';
+import express from "express";
+import router from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', router);
+app.use("/", router);
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
