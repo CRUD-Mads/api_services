98e326b1d46512f6dbb88abdf4ffa54e2c844841
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-06-11 09:16:15 -0300

    fix: :sparkles: cors config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import router from "./routes";
 import { testConnection } from "./services/db";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use("/", router);
 
